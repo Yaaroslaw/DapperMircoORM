a6140b1e44c9313b4e886d774c3d5943ec4f6a94
Yaaroslaw <yaaroslaw@gmail.com>
2017-11-30 16:44:42 +0200

    Added a class for the first helper "Execute a query and map ther results to a strongly typed List"

---
+++ DapperMircoORM/Helpers/ExecuteQueryMapToStronglyTypedList.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DapperMircoORM.Helpers
+{
+    /// <summary>
+    /// Execute a query and map the results to a strongly typed List
+    /// public static IEnumerable<T> Query<T>(this IDbConnection cnn, string sql, object param = null, SqlTransaction transaction = null, bool buffered = true)
+    /// </summary>
+    static class ExecuteQueryMapToStronglyTypedList
+    {
+        public class Dog
+        {            
+            public int? Age { get; set; }
+            public Guid Id { get; set; }
+            public string Name { get; set; }
+            public float? Weight { get; set; }
+
+            public int IgnoredProperty { get { return 1; } }
+        }
+    }
+}
--- DapperMircoORM/Program.cs
+++ DapperMircoORM/Program.cs
@@ -1,15 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using static DapperMircoORM.Helpers.ExecuteQueryMapToStronglyTypedList;
+
 
 namespace DapperMircoORM
 {
-    class Program
+    /// <summary>
+    /// Dapper is a NuGet library that you can add in to your project 
+    /// that will extend your IDbConnection interface. It provides 3 helpers
+    /// See folder "Helpers"
+    /// </summary>
+    public class Program
     {
         static void Main(string[] args)
         {
+            var guid = Guid.NewGuid();
+            var dog = connection.Query<Dog>("select Age = @Age, Id = @Id", new { Age = (int?)null, Id = guid });
+
+            Console.WriteLine("Dogs count should be 1, actually is: " + dog.Count());
+            Console.WriteLine("First dog age should be NULL, actually is: " + dog.First().Age);
+            //TODO: STRING INTERPOLATION
+            Console.WriteLine("First dog id should be: " + guid.ToString() + " actually is: " + dog.First().Id);
+
         }
     }
 }
