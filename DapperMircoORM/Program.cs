03a02795f3936bac38c18e5c4b3df2a498c64e0d
Yaaroslaw <yaaroslaw@gmail.com>
2017-11-30 15:58:19 +0200

    Add project files.

---
+++ DapperMircoORM/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DapperMircoORM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
