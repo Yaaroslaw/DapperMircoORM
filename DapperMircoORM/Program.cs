7ebcc150c2bd310b55a74db6603ded3643da7688
Yaaroslaw <yaaroslaw@gmail.com>
2017-12-05 13:56:19 +0200

    Code enhacement: String interpolation.

--- DapperMircoORM/Program.cs
+++ DapperMircoORM/Program.cs
@@ -22,10 +22,9 @@ namespace DapperMircoORM
             var guid = Guid.NewGuid();
             var dog = connection.Query<Dog>("select Age = @Age, Id = @Id", new { Age = (int?)null, Id = guid });
 
-            Console.WriteLine("Dogs count should be 1, actually is: " + dog.Count());
-            Console.WriteLine("First dog age should be NULL, actually is: " + dog.First().Age);
-            //TODO: STRING INTERPOLATION
-            Console.WriteLine("First dog id should be: " + guid.ToString() + " actually is: " + dog.First().Id);
+            Console.WriteLine($"Dogs count should be 1, actually is: {dog.Count()}");
+            Console.WriteLine($"First dog age should be NULL, actually is: {dog.First().Age}");
+            Console.WriteLine($"First dog id should be: {guid.ToString()} actually is: {dog.First().Id}");
 
         }
     }
