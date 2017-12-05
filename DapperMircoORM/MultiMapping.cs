cce40842863bd54d803f08af7d4c09fbbc975d5b
Yaaroslaw <yaaroslaw@gmail.com>
2017-12-05 16:01:36 +0200

    Added TestMultiMapping method.

--- DapperMircoORM/MultiMapping.cs
+++ DapperMircoORM/MultiMapping.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace DapperMircoORM
 {
@@ -20,6 +21,22 @@ namespace DapperMircoORM
         {
             public int Id { get; set; }
             public string Name { get; set; }
+        }
+
+        public void TestMultiMapping()
+        {
+            var sql =
+                    @"select * from #Posts p 
+                    left join #Users u on u.Id = p.OwnerId 
+                    Order by p.Id";
+
+            var data = connection.Query<Post, User, Post>(sql, (post, user) => { post.Owner = user; return post; });
+            var post = data.First();
+
+            Assert.AreEqual("Sams Post1", post.Content);
+            Assert.AreEqual(1, post.Id);
+            Assert.AreEqual("Sam", post.Owner.Name);
+            Assert.AreEqual(99, post.Owner.Id);
         }
 
     }
